47dcea43b28766aa534302223e4b1b23547cf0f1
syavingc <syaving@outlook.com>
2018-07-16 15:14:24 +0800

    111

---
+++ 【2】存储/S3/local_sync_s3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf /opt/eosbackup/*.tar.gz
+#
+storebp=`date +%F\-%H_%M_%S`
+#aws s3api put-object --bucket eosstorebp --key ${storebp}/ --region ap-northeast-1 |tee /root/syncs3.log  2>&1
+#tar
+cd /data/eos-data/
+tar zcvf /opt/eosbackup/${storebp}.tar.gz .
+#sync_s3
+cd /opt/eosbackup/
+aws s3 sync . s3://eosstorebp/ --acl public-read
